309d66c4bdf94597f821523d7e891a1e0b0a7fb0
Cleyton Caminha <cleytoncaminha01@gmail.com>
2021-06-10 19:18:25 -0300

    script with update post 2

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const app = express();
 const posts = require('../model/posts');
 
-express().use(express.json())
+app.use(express.json())
 
 
 router.get("/all",(req,res)=>[
